6eb5641ae747c3b0267704c834f889c9a5965248
Evgeny Butkevich <ebutkevich@shift4.com>
2022-01-20 08:43:11 +0300

    add routing components: about, main, sign-in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AboutComponent } from './components/routing/about/about.component';
+import { MainComponent } from './components/routing/main/main.component';
+import { SignInComponent } from './components/routing/sign-in/sign-in.component';
+
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
